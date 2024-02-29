c9677cbd1adbd9805615eda69e6af59b6651ea57
andreypisklov <c4mpunvrst@gmail.com>
2024-02-29 21:18:56 +0600

    check commit and push

--- app/src/main/java/io/caamp888/myapplication/SecondActivity.kt
+++ app/src/main/java/io/caamp888/myapplication/SecondActivity.kt
@@ -19,6 +19,7 @@ class SecondActivity : ComponentActivity() {
             MyApplicationTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+
                 }
             }
         }
